fac0d86ae3269d8106740d56ee0c706d2739b2f8
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-03 21:55:09 +0200

    A

--- settings.py
+++
@@ -1,7 +0,0 @@
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Iron/11.0.700.2 Chrome/11.0.700.2 Safari/534.24'
-FEED_FORMAT = 'csv'
-FEED_EXPORT_ENCODING = 'utf-8'
-
-# FEED_EXPORTERS = {
-#     'xml': 'postsScrapers.exporters.exporters.MyXmlExportPipeline'
-# }
