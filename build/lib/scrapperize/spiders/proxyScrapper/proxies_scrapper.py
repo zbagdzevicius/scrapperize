79e3f2e5bf72a73befe45bb5eb4b7e39bef7b79f
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-31 18:14:39 +0300

    A

--- build/lib/scrapperize/spiders/proxyScrapper/proxies_scrapper.py
+++ build/lib/scrapperize/spiders/proxyScrapper/proxies_scrapper.py
@@ -47,28 +47,6 @@ def crawl_proxies_from_urls_with_csv_format():
     proxies = '\n'.join(proxies)
     return proxies
 
-def crawl_proxies_from_proxybroker():
-    async def show(proxies):
-        while True:
-            proxy = await proxies.get()
-            if proxy is None: break
-
-    proxies = asyncio.Queue()
-    broker = Broker(proxies)
-    tasks = asyncio.gather(
-        broker.find(types=['HTTP', 'HTTPS'], limit=1000000),
-        show(proxies))
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(tasks)
-    proxy_list = []
-    for key, value in broker.unique_proxies.items():
-        proxy = f'{key[0]}:{key[1]}'
-        proxy_list.append(proxy)
-    proxies = '\n'.join(proxy_list)
-    return proxies
-
-
 def crawl_proxies():
     FEED_URI = f"proxies.csv"
     settings = get_project_settings()
@@ -81,9 +59,7 @@ def crawl_proxies():
     )
     crawler.start()
     crawled_proxies_from_urls = crawl_proxies_from_urls()
-    crawled_proxies_from_proxy_broker = crawl_proxies_from_proxybroker()
     with open(FEED_URI, 'a+') as file:
         file.write('\n'+crawled_proxies_from_urls)
-        file.write('\n'+crawled_proxies_from_proxy_broker)
         
             
